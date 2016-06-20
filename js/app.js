f3fbf3802b53468c56cf0307e6de2f7811c9a123
Max Dobler <mdblr@outlook.com>
2016-06-19 18:47:55 -0700

    use ngRoute for controller and view selection

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular
-  .module('simpleRedditClone', ['ngAnimate']);
+  angular
+    .module('simpleRedditClone', ['ngAnimate', 'ngRoute'])
+    .config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function config($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: '../templates/home.html',
+          controller: 'loadPageCtrl',
+          controllerAs: 'home'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+      $locationProvider.html5Mode(true);
+    }
+
+})();
